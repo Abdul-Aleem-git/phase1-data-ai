59c6862cf0afcaf5a58a8bb05150425e8a747ae2
Abdul Aleem <aleemchaudhary12a@gmail.com>
2026-01-09 22:13:52 +0500

    Day 3: Numpy indexing, slicing and Boolean masking

--- practice/numpy_basics.py
+++ practice/numpy_basics.py
@@ -38,3 +38,34 @@ data = np.array([10, 20, np.nan, 40, 50])
 
 print("Data:", data)
 print("Mean data(no nan):", np.nanmean(data))
+
+# Basic indexing
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("\nIndexing:")
+print("First Element:", arr[0])
+print("Last Element:", arr[-1])
+
+# Basic slicing
+
+print("\nSlicing:")
+print("First three elements:", arr[0:3])
+print("From 2nd element 2 to end:", arr[2:])
+
+# Boolean Masking
+
+print("\nBoolean Masking")
+
+mask = arr>25
+
+print("Mask:", mask)
+print("Values greater than 25:", arr[mask])
+
+# Real life example
+
+sales = np.array([120000, 80000, 100000, 50000, 90000])
+
+high_sales = sales>80000
+
+print("\nHigh sales values:", sales[high_sales])
